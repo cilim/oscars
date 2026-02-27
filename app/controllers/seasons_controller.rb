2c0d32bf676242279c79174d7fca0e4745c198d0
Marko Ćilimković <marko.cilimkovic@infinum.com>
2026-02-27 21:54:28 +0100

    Improve season show page

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -6,5 +6,7 @@ class SeasonsController < ApplicationController
   def show
     @season = Season.find(params[:id])
     @player = Current.user.players.find_by(season: @season)
+    @season_categories = @season.season_categories.includes(:category, :nominees, winner: :nominee)
+    @picks_by_category = @player&.picks&.includes(:think_will_win, :want_to_win)&.index_by(&:season_category_id) || {}
   end
 end
